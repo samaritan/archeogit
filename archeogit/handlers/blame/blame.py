8529f475f6b9af4f883176c7f52e12fa8ba1a432
Nuthan Munaiah <nuthan.munaiah@live.com>
2019-11-04 16:01:52 -0500

    Fix bug with repeated suspect lines

--- archeogit/handlers/blame/blame.py
+++ archeogit/handlers/blame/blame.py
@@ -1,5 +1,9 @@
+import logging
+
 from ... import utilities
 from .formatters import CSVFormatter, PlaintextFormatter
+
+logger = logging.getLogger(__name__)
 
 
 def _get_suspects(sections):
@@ -10,7 +14,7 @@ def _get_suspects(sections):
             suspects.extend(section.footer)
         else:
             suspects.extend(section.body['-'])
-    return suspects
+    return sorted(list(set(suspects)))
 
 
 class BlameHandler:
@@ -24,6 +28,8 @@ class BlameHandler:
         commits = dict()
         for path in sections:
             suspects = _get_suspects(sections[path])
+            logger.debug(sections[path])
+            logger.debug(suspects)
             commits[path] = self._repository.blamelines(
                 self._commit, path, suspects
             )
