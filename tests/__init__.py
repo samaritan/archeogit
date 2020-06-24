700102fceba4fc92332540ae38cbe4cf0bbd08e7
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-06-24 13:16:20 -0400

    Add a custom decorator to wrap around skipif
    The `pytest.mark.skipif` decorator allows conditional skipping of test
    functions. The custom decorator simply uses skipif to define a decorator
    that is contextual.

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,6 +5,17 @@ DATA_ROOT = os.path.join(os.path.dirname(__file__), 'data')
 _REASON = 'Test repository not found. Use `git submodule` to clone.'
 
 
+def requirerepositoryfortest(name):
+    def wrapper(function):
+        path = os.path.join(DATA_ROOT, name, '.git')
+
+        @pytest.mark.skipif(not os.path.exists(path), reason=_REASON)
+        def inner(*args, **kwargs):
+            function(*args, **kwargs)
+        return inner
+    return wrapper
+
+
 def requirerepositoryformodule(name):
     if not os.path.exists(os.path.join(DATA_ROOT, name, '.git')):
         pytest.skip(_REASON, allow_module_level=True)
