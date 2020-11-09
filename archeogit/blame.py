1438ba0d3ad5326747a8c2857e2fdec961b96072
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-11-09 15:54:13 -0500

    Refactor blame command handler to widen usage
    The encapsulation of the algorithm to blame a commit by BlameHandler
    prevented `archeogit` from being used as a Python module. The change
    refactors BlameHandler.handle to have the algorithm to blame in its own
    module making it usable as a module outside of the command line
    interface.

---
+++ archeogit/blame.py
@@ -0,0 +1,23 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def _get_suspects(sections):
+    suspects = list()
+    for section in sections:
+        if not section.body['-']:  # No lines were deleted in this section
+            suspects.extend(section.header)
+            suspects.extend(section.footer)
+        else:
+            suspects.extend(section.body['-'])
+    return sorted(list(set(suspects)))
+
+
+def blame(repository, commit):
+    commits = dict()
+    sections = repository.get_sections(commit)
+    for path in sections:
+        suspects = _get_suspects(sections[path])
+        commits[path] = repository.blamelines(commit, path, suspects)
+    return commits
--- archeogit/handlers/blame/blame.py
+++ archeogit/handlers/blame/blame.py
@@ -1,20 +1,9 @@
 import logging
 
-from ... import utilities
+from ... import blame, utilities
 from .formatters import CSVFormatter, PlaintextFormatter
 
 logger = logging.getLogger(__name__)
-
-
-def _get_suspects(sections):
-    suspects = list()
-    for section in sections:
-        if not section.body['-']:  # No lines were deleted in this section
-            suspects.extend(section.header)
-            suspects.extend(section.footer)
-        else:
-            suspects.extend(section.body['-'])
-    return sorted(list(set(suspects)))
 
 
 class BlameHandler:
@@ -24,16 +13,7 @@ class BlameHandler:
         self._csv = csv
 
     def handle(self):
-        sections = self._repository.get_sections(self._commit)
-        commits = dict()
-        for path in sections:
-            suspects = _get_suspects(sections[path])
-            logger.debug(sections[path])
-            logger.debug(suspects)
-            commits[path] = self._repository.blamelines(
-                self._commit, path, suspects
-            )
-
+        commits = blame.blame(self._repository, self._commit)
         formatter = PlaintextFormatter()
         if self._csv:
             formatter = CSVFormatter()
