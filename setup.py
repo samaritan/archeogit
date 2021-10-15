63cf9e674181eca67c45b5b1696eadcf375cd077
Nuthan Munaiah <nuthan.munaiah@live.com>
2021-10-15 15:45:27 -0400

    Add `VERSION` to the repository
    The `VESION` file will contain the current version of `archeogit`.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+VERSION = None
+with open("VERSION", "r") as file_:
+    VERSION, _ = file_.read().split('-')
+
 setup(
     name='archeogit',
-    version='0.2.1',
+    version=VERSION,
     packages=['archeogit'],
     install_requires=['jinja2~=2.10', 'pygit2~=0.28'],
     entry_points={
